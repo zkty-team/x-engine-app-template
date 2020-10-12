b3063fa27bac40718079a35788c0efec11890343
zk <liuzq7@gmail.com>
2020-10-12 19:28:26 +0800

    init

---
+++ iOS/ModulePods.rb
@@ -0,0 +1,10 @@
+
+require "json"
+def module_pods
+  pod 'x-engine-module-device', :path =>'../node_modules/@zkty-team/x-engine-module-device'
+  pod 'x-engine-module-engine', :path =>'../node_modules/@zkty-team/x-engine-module-engine'
+  pod 'x-engine-module-network', :path =>'../node_modules/@zkty-team/x-engine-module-network'
+  pod 'x-engine-module-protocols', :path =>'../node_modules/@zkty-team/x-engine-module-protocols'
+  
+end
+  
