56d8448a1189c62050f4a7b93941faa652cfd854
zk <liuzq7@gmail.com>
2020-10-14 00:27:56 +0800

    update

--- iOS/ModulePods.rb
+++ iOS/ModulePods.rb
@@ -1,4 +1,13 @@
 
-pod 'x-engine-module-device', :path =>'../node_modules/@zkty-team/x-engine-module-device'
-pod 'x-engine-module-engine', :path =>'../node_modules/@zkty-team/x-engine-module-engine'
-pod 'x-engine-module-network', :path =>'../node_modules/@zkty-team/x-engine-module-network'
+require "json"
+def module_pods
+  pod 'x-engine-module-device', :path =>'../node_modules/@zkty-team/x-engine-module-device'
+  pod 'x-engine-module-engine', :path =>'../node_modules/@zkty-team/x-engine-module-engine'
+  pod 'x-engine-module-microapp', :path =>'../node_modules/@zkty-team/x-engine-module-microapp'
+  pod 'x-engine-module-network', :path =>'../node_modules/@zkty-team/x-engine-module-network'
+  pod 'x-engine-module-protocols', :path =>'../node_modules/@zkty-team/x-engine-module-protocols'
+  pod 'x-engine-module-tools', :path =>'../node_modules/@zkty-team/x-engine-module-tools'
+  pod 'x-engine-module-ui', :path =>'../node_modules/@zkty-team/x-engine-module-ui'
+  
+end
+  
